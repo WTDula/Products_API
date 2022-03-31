f80fd75ebb861bf2bb5d2e261bd1c836fe0ac85a
WTDula <wtdula@gmail.com>
2022-03-31 14:34:16 -0500

    added POST functionality in views.py and tested in Postman, added coerce decimal to string to false to display price as a float

--- products/views.py
+++ products/views.py
@@ -2,11 +2,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from django.shortcuts import get_list_or_404
+from rest_framework import status
 
 # Create your views here.
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def products_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
