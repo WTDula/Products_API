6465d1769170c8df88fcf88b693f67125474073b
WTDula <wtdula@gmail.com>
2022-03-31 14:45:16 -0500

    created new view function for getting product y id, added new path for product_detail view function

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework import status
 
 # Create your views here.
@@ -19,3 +19,9 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
+@api_view(["GET"])
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk = pk)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
