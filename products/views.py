51f395ce9af3b48cf4e58db6bb08da66c5919db9
WTDula <wtdula@gmail.com>
2022-04-01 13:58:09 -0500

    added reviews to installed apps in settings.py, added review path for GET endpoint in products_project.urls.py and reviews app urls.py, created ReviewSerializer, added logic for GET in reviews_list in reviews.views,py

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
-from django.shortcuts import get_list_or_404, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 
 # Create your views here.
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Review
+from .serializers import ReviewSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
-# Create your views here.
+@api_view(["GET"])
+def reviews_list(request):
+    if request.method == "GET":
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many = True)
+        return Response(serializer.data)
